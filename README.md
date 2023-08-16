6a4921e2a6bd16b78863adf322bc5a307bf06dfb
YassineGhezal <85171806+Ryusuke66@users.noreply.github.com>
2023-08-16 13:01:00 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Blogr landing page solution
+
+This is a solution to the [Blogr landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+## Desktop design
+
+<p float="left">
+  <img src="./screenshots/desktop-design-1.png" width="50%" />
+  <img src="./screenshots/desktop-design-2.png" width="50%" />
+</p>
+
+## Mobile design
+
+<p float="left">
+  <img src="./screenshots/mobile-design-3.png" width="33%" />
+  <img src="./screenshots/mobile-design-1.png" width="33%" />
+  <img src="./screenshots/mobile-design-2.png" width="33%" />
+</p>
+
+### Links
+
+- Live Site URL: [Blogr landing page](https://ryusuke66.github.io/blogr-landing-page)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Pure JavaScript
+
+## Author
+
+- Frontend Mentor - [@Ryusuke66](https://www.frontendmentor.io/profile/ryusuke66)
---
+++ script.js
@@ -0,0 +1,70 @@
+let menuIcon = document.getElementById("menu"),
+    body = document.querySelector("body"),
+    nav = document.querySelector(".mobile-nav");
+
+menuIcon.addEventListener("click", () => {
+    if (menuIcon.classList.contains("active")) {
+        menuIcon.classList.remove("active");
+        menuIcon.classList.add("inactive");
+        nav.classList.remove("on");
+    } else {
+        menuIcon.classList.add("active");
+        menuIcon.classList.remove("inactive");
+        nav.classList.add("on");
+        window.scrollTo({
+            top:0,
+            left:0,
+            behavior: "smooth"
+        })
+    }
+})
+
+body.addEventListener("click", () => {
+    if (menuIcon.classList.contains("active")) {
+        nav.classList.remove("on");
+        menuIcon.classList.remove("active");
+        menuIcon.classList.add("inactive");
+        body.style.overflowY = "auto";
+    };
+})
+menuIcon.addEventListener("click", (ev)  => {
+    ev.stopPropagation();
+});
+nav.addEventListener("click", (ev) => {
+    ev.stopPropagation();
+});
+
+let drop = document.querySelectorAll(".mobile-nav .drop"),
+    dropBar = document.querySelectorAll(".mobile-nav .dropbar");
+
+for (let i = 0; i < drop.length; i++) {
+    drop[i].addEventListener("click", () => {
+        if(drop[i].classList.contains("on")) {
+            drop[i].classList.remove("on");
+        } else {
+            drop[i].classList.add("on");
+        }
+    })
+    dropBar[i].addEventListener("click", (e) => {
+        e.stopPropagation();
+    })
+}
+
+let desktopNav = document.querySelector(".desktop-nav"),
+    desktopDrop = document.querySelectorAll(".desktop-nav .drop"),
+    desktopDropBar = document.querySelectorAll(".desktop-nav .dropbar");
+
+for (let i = 0; i < desktopDrop.length; i++) {
+    desktopDrop[i].addEventListener("click", () => {
+        if(desktopDrop[i].classList.contains("on")) {
+            desktopDrop[i].classList.remove("on");
+        } else {
+            desktopDrop.forEach(a => a.classList.remove("on"));
+            desktopDrop[i].classList.add("on");
+        }
+    })
+    desktopDropBar[i].addEventListener("click", (e) => {
+        e.stopPropagation();
+    })
+}
+
